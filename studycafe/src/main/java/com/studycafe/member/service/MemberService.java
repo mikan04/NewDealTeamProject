62489a7efd9b9a93958af267c38cdba8507d7d59
김용주 <kyju9568@naver.com>
2023-07-17 17:08:47 +0900

    회원가입 중

--- studycafe/src/main/java/com/studycafe/member/service/MemberService.java
+++ studycafe/src/main/java/com/studycafe/member/service/MemberService.java
@@ -1,5 +1,13 @@
 package com.studycafe.member.service;
 
-public class MemberService {
+import com.studycafe.member.entity.MemberEntity;
+
+public interface MemberService {
+	
+	public void insertMember(MemberEntity member);
+	
+	public MemberEntity loginService(MemberEntity member);
+	
+	
 
 }
