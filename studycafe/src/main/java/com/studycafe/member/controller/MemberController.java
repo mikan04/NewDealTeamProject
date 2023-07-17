d6635df7bea5198c5e4a4ad074f1d2b6d6d2267f
김용주 <kyju9568@naver.com>
2023-07-17 18:06:46 +0900

    entity 수정

--- studycafe/src/main/java/com/studycafe/member/controller/MemberController.java
+++ studycafe/src/main/java/com/studycafe/member/controller/MemberController.java
@@ -1,13 +1,20 @@
 package com.studycafe.member.controller;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.studycafe.member.entity.MemberEntity;
+
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
 @Controller
 public class MemberController {
 	
@@ -18,8 +25,14 @@ public class MemberController {
 	}
 	
 	@GetMapping("/joinForm")
-	public String joinForm1() {
+	public String joinForm() {
 		return "member/joinForm";
+	}
+	
+	@PostMapping("/joinPro")
+	public String joinPro(MemberEntity memberEn, HttpServletRequest request) {
+		log.info(memberEn);
+		return "redirect:member/loginForm";
 	}
 
 }
