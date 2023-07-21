b1d8c25e52d966ddd3e250e644ebbbd6fd277284
Daehwan Lee <eoghks1703@gmail.com>
2023-07-21 09:22:55 +0900

    2023-07-20 (대환) 스터디 등록 시간 설정 업데이트

--- studycafe/src/main/java/com/studycafe/study/repository/StudyRepository.java
+++ studycafe/src/main/java/com/studycafe/study/repository/StudyRepository.java
@@ -1,8 +1,13 @@
 package com.studycafe.study.repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.studycafe.study.entity.StudyEntity;
@@ -11,4 +16,9 @@ import com.studycafe.study.entity.StudyEntity;
 public interface StudyRepository extends JpaRepository<StudyEntity, Integer> {
 
 	Page<StudyEntity> findBystudyTitleContaining(String keyword, Pageable pageable);
+	
+	@Query(value="SELECT * FROM study_entity WHERE longitude= :longitude and latitude= :latitude and reserve_date= :date", nativeQuery = true)
+	List<StudyEntity> findByMap(@Param("longitude") int longitude, @Param("latitude") int latitude , @Param("date") LocalDate date);
+	
+	
 }
