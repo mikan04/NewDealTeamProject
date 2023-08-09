53a938834b2e262fec76763d0a0322237e8f1bb6
Jae Kuk <sasameyuki03@naver.com>
2023-08-09 18:17:49 +0900

    2023-08-09 (재국) 팀 해체 구현중 매우 어려움

--- studycafe/src/main/resources/static/js/myTeam.js
+++ studycafe/src/main/resources/static/js/myTeam.js
@@ -1,19 +1,19 @@
-window.resizeTo(1000,600);
+window.resizeTo(1000, 700);
 
 function getOutTeam(data) {
 
 	let teamNumber = $("#teamNumber").val();
-	
+
 	let teamHead = $("#teamHead").val();
-	
-	if(teamHead === data){
-		alert ("팀장은 내보낼 수 없습니다.");
-		
+
+	if (teamHead === data) {
+		alert("팀장은 내보낼 수 없습니다.");
+
 		return false;
 	}
 
-
 	console.log(teamNumber);
+
 	$.ajax({
 		type: "post",
 		url: "/team/getoutteam",
@@ -23,26 +23,42 @@ function getOutTeam(data) {
 		},
 		dataType: "json",
 		success: function(data) {
+
 			alert("내보내기 성공");
-			location.reload();
+			location.reload(true);
+
 		}
 	})
 }
 
-function deleteTeam(){
-	let teamNumber = $("#teamNumber").val();
+function deleteTeam(teamNumber) {
 
 	$.ajax({
-		type: "post",
+		type: "DELETE",
 		url: "/team/delete",
 		data: {
 			"teamNumber": teamNumber
 		},
 		dataType: "json",
+
 		success: function(data) {
-			alert("팀을 삭제하였습니다.");
-			window.close();
-			location.href("/");
+
+			if (data) {
+				alert("팀을 해체하였습니다.");
+
+				location.href = "/logout";
+				
+				window.close();
+				
+			} else {
+				alert("알 수 없는 에러가 발생하였습니다.");
+				
+				return false;
+			}
+
+		},
+		error : function(error){
+			alert("관리자에게 문의해주세요. 팀 삭제 도중 알 수 없는 에러가 발생하였습니다.");
 		}
 	})
 
