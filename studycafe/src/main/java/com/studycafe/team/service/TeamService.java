cc66b3a51445d6ae0a3beaf5f8dd5ff2209f921f
Jae Kuk <sasameyuki03@naver.com>
2023-08-10 16:35:03 +0900

    2023-08-10 (재국) 관리자 점수 부여시 팀 포인트 증가 및 css 깨짐 수정

--- studycafe/src/main/java/com/studycafe/team/service/TeamService.java
+++ studycafe/src/main/java/com/studycafe/team/service/TeamService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.studycafe.team.dto.TeamMonthCountDto;
 import com.studycafe.team.dto.TopTeamDto;
+import com.studycafe.team.entity.ResultAuthEntity;
 import com.studycafe.team.entity.TeamEntity;
 
 public interface TeamService {
@@ -20,6 +21,7 @@ public interface TeamService {
 	public TeamEntity getMyTeam(long teamNumber);
 	public void deleteTeam(long teamNumber);
 	public List<TeamEntity> getRanking();
+	public void updatePoint(ResultAuthEntity resultAuthEntity);
 
 	
 
